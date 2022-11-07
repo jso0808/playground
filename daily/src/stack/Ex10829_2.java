a1805b8f060f301e77d877c891c908b7fab6a0dc
jso0808 <A@DESKTOP-JEONG>
2022-11-07 19:55:56 +0900

    9093번 - 스택, StringBuilder-reverse()

--- daily/src/stack/Ex10829_2.java
+++ daily/src/stack/Ex10829_2.java
@@ -1,4 +1,4 @@
-package boj;
+package stack;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
---
+++ daily/src/stack/Ex9093_2.java
@@ -0,0 +1,36 @@
+package stack;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex9093_2 {
+
+	public static void main(String[] args) {
+		
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			StringBuffer sb = new StringBuffer();
+			int cnt = Integer.parseInt(br.readLine());
+			
+			while(cnt-- > 0) {
+				String sentance = br.readLine();
+				StringTokenizer token = new StringTokenizer(sentance);
+				
+				while(token.hasMoreElements()) {
+					StringBuffer re = new StringBuffer(token.nextToken());
+					sb.append(re.reverse());
+					sb.append(" ");
+				}
+			}
+			
+			System.out.println(sb);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
