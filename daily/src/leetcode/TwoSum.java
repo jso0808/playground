4eb98a7b907ffb523a828208dc26ba47cfad8a63
jso0808 <A@192.168.219.104>
2022-11-19 15:50:58 +0900

    leetCode - TwoSum

---
+++ daily/src/leetcode/TwoSum.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum {
+	public int[] twoSum(int[] nums, int target) {
+		/*
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] + nums[j] == target) {
+					return new int[] { i, j };
+				}
+			}
+		}
+		*/
+		
+		HashMap<Integer, Integer> map = new HashMap<>();
+		
+		for(int i=0; i<nums.length; i++) {
+			map.put(nums[i], i);
+		}
+		
+		for(int i=0; i<nums.length; i++) {
+			Integer pre = map.get(target - nums[i]);
+			if(pre != null && pre != i) {
+				return new int[] {i, pre};
+			}
+		}
+		
+		return new int[] {};
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { 1, 4, 7, 8 };
+		int target = 11;
+
+		TwoSum t = new TwoSum();
+		System.out.println(Arrays.toString(t.twoSum(nums, target)));
+	}
+}
