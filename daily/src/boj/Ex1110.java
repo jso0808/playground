6f3c24b5d597252bf3e121c3a5a7329590a0db26
jso0808 <A@192.168.219.103>
2022-11-22 00:07:12 +0900

    백준 - 1110, 1940번

--- daily/src/boj/Ex1110.java
+++ daily/src/boj/Ex1110.java
@@ -8,8 +8,22 @@ public class Ex1110 {
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			
-			int first = Integer.parseInt(br.readLine());
+			int num = Integer.parseInt(br.readLine());
+			int n = num;
+			int cnt = 0;
+			int temp=0;
+			boolean b = true;
 			
+			while(b) {
+				temp = n/10 + n%10; // 2+6=8
+				n = n%10*10 + temp%10; // 60+8=68
+				cnt++;
+				if(n==num) {
+					b = false;
+				}
+			}
+			
+			System.out.println(cnt);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
